3e3b67a76f419a6287e54709789c0237a7f307e6
Penguin <say8425@gmail.com>
2018-06-18 19:01:59 +0900

    Find accesskey and secretkey from credntial profile

--- lib/ec2_tag_env.rb
+++ lib/ec2_tag_env.rb
@@ -1,8 +1,19 @@
+require 'aws_config'
+
 class Ec2TagEnv < Thor
   desc 'Export environment values from Tag. If you use it withour key or profile, then Ec2TagEnv will ask you keys.'
   method_option :access_key, aliases: '-a', desc: 'You can type the access key manually.'
   method_option :secret_key, aliases: '-s', desc: 'You can type the secret key manually.'
   method_option :profile, aliases: '-p', desc: 'You can type the aws credential profile.'
   def export
+    profile = options[:profile]
+
+    keys = if profile
+             {
+               access_key: AWSConfig[profile].aws_access_key_id,
+               secret_key: AWSConfig[profile].aws_secret_access_key,
+             } 
+           else
+           end
   end
 end
