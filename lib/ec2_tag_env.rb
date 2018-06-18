5f01ab8c5f5328a843163a0401d95178a25d3402
Penguin <say8425@gmail.com>
2018-06-18 18:51:42 +0900

    init export command

--- lib/ec2_tag_env.rb
+++ lib/ec2_tag_env.rb
@@ -1,4 +1,8 @@
-class Ec2TagEnv
-  def initialize
+class Ec2TagEnv < Thor
+  desc 'Export environment values from Tag. If you use it withour key or profile, then Ec2TagEnv will ask you keys.'
+  method_option :access_key, aliases: '-a', desc: 'You can type the access key manually.'
+  method_option :secret_key, aliases: '-s', desc: 'You can type the secret key manually.'
+  method_option :profile, aliases: '-p', desc: 'You can type the aws credential profile.'
+  def export
   end
 end
